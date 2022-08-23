13e7bdab06bacad993dbc6aa98e0ee836c9915b8
bigbade <bigbade333@gmail.com>
2022-08-23 10:36:38 -0500

    Added code components

---
+++ Compiler/Components/MiscComponents.cs
@@ -0,0 +1,47 @@
+﻿namespace Compiler.Components
+{
+    public class MiscComponents
+    {
+        public class ColorComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_r", "signal_g", "signal_b", "signal_a"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class DelayComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in", "set_delay"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class MemoryComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in", "lock_state"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class OscillatorComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"set_frequency", "set_outputtype"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class RelayComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in_1", "signal_in_2", "toggle_state", "set_state"};
+
+            public string[] OutputConnections() => new[] {"signal_out_1", "signal_out_2", "state_out", "load_value_out", "power_value_out"};
+        }
+        
+        public class WifiComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in", "set_channel", "signal_out"};
+
+            public string[] OutputConnections() => new[] {"signal_in", "signal_out"};
+        }
+    }
+}
---
+++ Compiler/Components/TrigComponents.cs
@@ -0,0 +1,47 @@
+﻿namespace Compiler.Components
+{
+    public class TrigComponents
+    {
+        public class AcosComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class AsinComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class AtanComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class CosComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class SinComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+        
+        public class TanComponent : IComponent
+        {
+            public string[] InputConnections() => new[] {"signal_in"};
+
+            public string[] OutputConnections() => new[] {"signal_out"};
+        }
+    }
+}
